bbb1a153df59e543fb402f21ee45cd46f3d8a2fa
Quentin Bastos <quentin.bastos@etu.univ-lyon1.fr>
2024-11-14 17:02:53 +0100

    feat(doc): add readme

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -685,7 +685,6 @@ export function RegisterRoutes(app: Router) {
             // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 
             try {
-                // @ts-ignore
                 request['user'] = await Promise.any(secMethodOrPromises);
 
                 // Response was sent in middleware, abort
