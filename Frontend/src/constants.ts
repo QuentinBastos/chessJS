868fa6a28a65717cb9b368824eb1993fa554d063
Quentin Bastos <quentin.bastos@etu.univ-lyon1.fr>
2024-12-11 09:40:58 +0100

    fix(const): update routes

--- Frontend/src/constants.ts
+++ Frontend/src/constants.ts
@@ -5,6 +5,6 @@ export const API_ROOT_URL = "/api";
 export const API_BOARD_URL = "/board";
 export const API_INIT_BOARD_URL = "/init-board";
 export const API_POSSIBLES_MOVES_URL = "/possible-moves/:id";
-export const API_MOVE_PIECE_URL = "/move-piece";
+export const API_MOVE_PIECE_URL = "/move-pieces";
 export const API_KING_CHECK_URL = "/is-king-in-check/:color";
 export const API_GAMES_URL = "/games";
